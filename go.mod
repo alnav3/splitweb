5649a866a8626bbe791fcc21d870de4c9c1a39cb
alnav3 <developer@apps.alnav.dev>
2024-06-29 21:20:18 +0200

    simple hello world to check the server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module splitweb
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+    // handle paths
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+    // handle directories for static files
+    handleDirectory("js")
+
+    // start the server
+	log.Println("Server started on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func handleDirectory(dirs... string) {
+    for _, dir := range dirs {
+        http.Handle(dir, http.StripPrefix(dir, http.FileServer(http.Dir("."+dir))))
+    }
+}
