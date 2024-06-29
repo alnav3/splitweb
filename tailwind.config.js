da0fe248b48a60d5e3c5ed9b4a488f567905a5ff
alnav3 <developer@apps.alnav.dev>
2024-06-29 22:33:51 +0200

    config files for tailwindcss and air

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'selector',
+  content: [ "./**/*.html", "./**/*.templ", "./**/*.go", ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      defaultFlavour: "latte",
+  }),
+  ],
+}
+
+
