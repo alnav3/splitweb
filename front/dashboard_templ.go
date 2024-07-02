5aa87ec104ccadc32c4fd4c2efc3a18472b94f82
alnav3 <developer@apps.alnav.dev>
2024-07-02 15:33:55 +0200

    error fixed

--- front/dashboard_templ.go
+++ front/dashboard_templ.go
@@ -45,7 +45,7 @@ func ThemeButton(theme string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"flex fixed top-0 right-0 p-12\"><button onclick=\"htmx.removeClass(htmx.find(&#39;#body&#39;), &#39;dark&#39;)\" hx-get=\"/theme\" hx-target=\"this\" hx-swap=\"none\"><img src=\"/img/icons/theme/light.svg\" alt=\"Icon\" class=\"w-9 h-9 mr-2 hidden dark:inline\"></button> <button onclick=\"htmx.addClass(htmx.find(&#39;#body&#39;), &#39;dark&#39;)\" hx-get=\"/theme\" hx-target=\"this\" hx-swap=\"none\"><img src=\"/img/icons/theme/dark.svg\" alt=\"Icon\" class=\"w-9 h-9 mr-2 dark:hidden\"></button> <button hx-get=\"/account\" hx-target=\"this\" hx-swap=\"none\"><svg class=\"fill-mauve w-9 h-9 mr-2 mx-3\" xmlns=\"http://www.w3.org/2000/svg\" height=\"32px\" viewBox=\"0 -960 960 960\" width=\"32px\" fill=\"#e8eaed\"><path d=\"M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z\"></path></svg></button></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"flex fixed top-0 right-0 p-4 md:p-12\"><button onclick=\"htmx.removeClass(htmx.find(&#39;#body&#39;), &#39;dark&#39;)\" hx-get=\"/theme\" hx-target=\"this\" hx-swap=\"none\"><img src=\"/img/icons/theme/light.svg\" alt=\"Icon\" class=\"w-9 h-9 mr-2 hidden dark:inline\"></button> <button onclick=\"htmx.addClass(htmx.find(&#39;#body&#39;), &#39;dark&#39;)\" hx-get=\"/theme\" hx-target=\"this\" hx-swap=\"none\"><img src=\"/img/icons/theme/dark.svg\" alt=\"Icon\" class=\"w-9 h-9 mr-2 dark:hidden\"></button> <button hx-get=\"/account\" hx-target=\"this\" hx-swap=\"none\"><svg class=\"fill-mauve w-9 h-9 mr-2 mx-3\" xmlns=\"http://www.w3.org/2000/svg\" height=\"32px\" viewBox=\"0 -960 960 960\" width=\"32px\" fill=\"#e8eaed\"><path d=\"M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z\"></path></svg></button></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -69,14 +69,14 @@ func Base(username string) templ.Component {
 			templ_7745c5c3_Var2 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html><head><title>Dashboard - ")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Dashboard - ")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var3 string
 		templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(username)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 50, Col: 32}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 52, Col: 32}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
 		if templ_7745c5c3_Err != nil {
@@ -186,7 +186,7 @@ func Projects(projects []Project) templ.Component {
 			var templ_7745c5c3_Var7 string
 			templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs(project.Title)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 85, Col: 72}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 87, Col: 72}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
 			if templ_7745c5c3_Err != nil {
@@ -204,7 +204,7 @@ func Projects(projects []Project) templ.Component {
 				var templ_7745c5c3_Var8 string
 				templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%.2f", project.Balance) + project.currencySymbol)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 89, Col: 90}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 91, Col: 90}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 				if templ_7745c5c3_Err != nil {
@@ -222,7 +222,7 @@ func Projects(projects []Project) templ.Component {
 				var templ_7745c5c3_Var9 string
 				templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%.2f", project.Balance) + project.currencySymbol)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 94, Col: 90}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 96, Col: 90}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var9))
 				if templ_7745c5c3_Err != nil {
@@ -240,7 +240,7 @@ func Projects(projects []Project) templ.Component {
 				var templ_7745c5c3_Var10 string
 				templ_7745c5c3_Var10, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("%.2f", project.Balance) + project.currencySymbol)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 99, Col: 90}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `front/dashboard.templ`, Line: 101, Col: 90}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var10))
 				if templ_7745c5c3_Err != nil {
